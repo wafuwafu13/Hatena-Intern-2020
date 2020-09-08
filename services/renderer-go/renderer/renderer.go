d13192ee98991aee81a5e43ff3fa034ee362e554
wafuwafu13 <wafuwafu1313@gmail.com>
2020-09-08 11:08:15 +0900

    goldmarkを導入した

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"html/template"
 	"regexp"
+	"github.com/yuin/goldmark"
 )
 
 var urlRE = regexp.MustCompile(`https?://[^\s]+`)
@@ -13,13 +14,17 @@ var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
 	// TODO: これはサンプル実装 (URL の自動リンク) です
-	html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-		var w bytes.Buffer
-		err := linkTmpl.ExecuteTemplate(&w, "link", url)
-		if err != nil {
-			return url
-		}
-		return w.String()
-	})
-	return html, nil
+	// html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
+	// 	var w bytes.Buffer
+	// 	err := linkTmpl.ExecuteTemplate(&w, "link", url)
+	// 	if err != nil {
+	// 		return url
+	// 	}
+	// 	return w.String()
+	// })
+	var buf bytes.Buffer
+	if err := goldmark.Convert([]byte(src), &buf); err != nil {
+		panic(err)
+	} 
+	return buf.String(), nil
 }
