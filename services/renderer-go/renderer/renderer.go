b1c84e326a9d343a2c298c61666d2f9cbe98b32f
wafuwafu13 <wafuwafu1313@gmail.com>
2020-09-08 17:16:05 +0900

    tasklistを実装

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -4,13 +4,136 @@ import (
 	"bytes"
 	"context"
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/renderer/html"
+	gast "github.com/yuin/goldmark/ast"
+	"github.com/wafuwafu13/goldmark-tasklist-extension/ast"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer"
+	"github.com/yuin/goldmark/text"
+	"github.com/yuin/goldmark/util"
+	"regexp"
+)
+
+var markdown = goldmark.New(
+	goldmark.WithRendererOptions(
+		html.WithXHTML(),
+		html.WithUnsafe(),
+	),
+	goldmark.WithExtensions(
+		TaskList,
+	),
 )
 
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
 	var buf bytes.Buffer
-	if err := goldmark.Convert([]byte(src), &buf); err != nil {
+	if err := markdown.Convert([]byte(src), &buf); err != nil {
 		return "", err
 	} 
 	return buf.String(), nil
+}
+
+var taskListRegexp = regexp.MustCompile(`^\[([\sxX])\]\s*`)
+
+type taskCheckBoxParser struct {
+}
+
+var defaultTaskCheckBoxParser = &taskCheckBoxParser{}
+
+// NewTaskCheckBoxParser returns a new  InlineParser that can parse
+// checkboxes in list items.
+// This parser must take precedence over the parser.LinkParser.
+func NewTaskCheckBoxParser() parser.InlineParser {
+	return defaultTaskCheckBoxParser
+}
+
+func (s *taskCheckBoxParser) Trigger() []byte {
+	return []byte{'['}
+}
+
+func (s *taskCheckBoxParser) Parse(parent gast.Node, block text.Reader, pc parser.Context) gast.Node {
+	// Given AST structure must be like
+	// - List
+	//   - ListItem         : parent.Parent
+	//     - TextBlock      : parent
+	//       (current line)
+	if parent.Parent() == nil || parent.Parent().FirstChild() != parent {
+		return nil
+	}
+
+	if _, ok := parent.Parent().(*gast.ListItem); !ok {
+		return nil
+	}
+	line, _ := block.PeekLine()
+	m := taskListRegexp.FindSubmatchIndex(line)
+	if m == nil {
+		return nil
+	}
+	value := line[m[2]:m[3]][0]
+	block.Advance(m[1])
+	checked := value == 'x' || value == 'X'
+	ranged := value == 'r' || value == 'R'
+	return ast.NewTaskCheckBox(checked, ranged)
+}
+
+func (s *taskCheckBoxParser) CloseBlock(parent gast.Node, pc parser.Context) {
+	// nothing to do
+}
+
+// TaskCheckBoxHTMLRenderer is a renderer.NodeRenderer implementation that
+// renders checkboxes in list items.
+type TaskCheckBoxHTMLRenderer struct {
+	html.Config
+}
+
+// NewTaskCheckBoxHTMLRenderer returns a new TaskCheckBoxHTMLRenderer.
+func NewTaskCheckBoxHTMLRenderer(opts ...html.Option) renderer.NodeRenderer {
+	r := &TaskCheckBoxHTMLRenderer{
+		Config: html.NewConfig(),
+	}
+	for _, opt := range opts {
+		opt.SetHTMLOption(&r.Config)
+	}
+	return r
+}
+
+// RegisterFuncs implements renderer.NodeRenderer.RegisterFuncs.
+func (r *TaskCheckBoxHTMLRenderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
+	reg.Register(ast.KindTaskCheckBox, r.renderTaskCheckBox)
+}
+
+func (r *TaskCheckBoxHTMLRenderer) renderTaskCheckBox(w util.BufWriter, source []byte, node gast.Node, entering bool) (gast.WalkStatus, error) {
+	if !entering {
+		return gast.WalkContinue, nil
+	}
+	n := node.(*ast.TaskCheckBox)
+
+	if n.IsChecked {
+		w.WriteString(`<input type="checkbox"`)
+	} else if n.IsRanged {
+		w.WriteString(`<input type="range"`)
+	} else {
+		w.WriteString(`<input disabled="" type="checkbox"`)
+	}
+	if r.XHTML {
+		w.WriteString(" /> ")
+	} else {
+		w.WriteString("> ")
+	}
+	return gast.WalkContinue, nil
+}
+
+type taskList struct {
+}
+
+// TaskList is an extension that allow you to use GFM task lists.
+var TaskList = &taskList{}
+
+func (e *taskList) Extend(m goldmark.Markdown) {
+	m.Parser().AddOptions(parser.WithInlineParsers(
+		util.Prioritized(NewTaskCheckBoxParser(), 0),
+	))
+	m.Renderer().AddOptions(renderer.WithNodeRenderers(
+		util.Prioritized(NewTaskCheckBoxHTMLRenderer(), 500),
+	))
 }
