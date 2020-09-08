2bc0ef77206a8ae54e09060d9076f57f13b23776
wafuwafu13 <wafuwafu1313@gmail.com>
2020-09-08 11:58:13 +0900

    テストを書いた

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -1,15 +1,40 @@
 package renderer
 
 import (
-	"context"
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/yuin/goldmark"
 )
 
 func Test_Render(t *testing.T) {
-	src := `foo https://google.com/ bar`
-	html, err := Render(context.Background(), src)
-	assert.NoError(t, err)
-	assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
+	tests := []struct {
+		input string
+		expected string
+	}{
+		{
+			`# foo`, 
+`<h1>foo</h1>
+`,
+},
+		{
+			`[Google](https://www.google.com/)`,
+`<p><a href="https://www.google.com/">Google</a></p>
+`,
+},
+		{
+			`- bar`, 
+`<ul>
+<li>bar</li>
+</ul>
+`,
+},
+	}
+
+	for _, tt := range tests {
+		var buf bytes.Buffer
+	    goldmark.Convert([]byte(tt.input), &buf)
+	    assert.Equal(t, tt.expected, buf.String())
+	}
 }
